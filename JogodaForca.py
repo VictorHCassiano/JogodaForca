0086acdae3b835a35d170a53326183cf4ec3653e
Victor Hugo <104807646+VictorHCassiano@users.noreply.github.com>
2023-05-17 13:05:03 -0300

    Update JogodaForca.py

--- JogodaForca.py
+++ JogodaForca.py
@@ -1,37 +1,36 @@
 import random as rd
-respostas = ["victor","gustavo","magnum","lucas","felipe"]
-resp = respostas[rd.randint(0,4)]
-tentativa = []
-print("Bem vindo ao jogo da forca ")
-print(resp)
-for x in resp:
-    tentativa.append("_")
-check = 0
+
+palavras = ["victor", "gustavo", "magnum", "lucas", "felipe"]
+palavra_secreta = palavras[rd.randint(0, 4)]
+letras_adivinhadas = []
 chances = 0
+for x in palavra_secreta:
+    letras_adivinhadas.append("_")
+print("Bem vindo ao jogo da forca")
+print(palavra_secreta)
 
 while True:
     if chances == 10:
         print("Voce perdeu")
-        break
-
-    print(f"Voce tem {10-chances} chances")
-    print(tentativa)
-    letraAdivinhada = input("Digite sua tentativa:")
-
-    for x in range(len(resp)):
-        if letraAdivinhada == resp[x]:
-            tentativa[x]= letraAdivinhada 
-    for y in tentativa:
-        
-        if y == "_":
-            check+=1
-       
-    if check == 0:
-        print(f"Voce venceu!!!\na resposta era {resp}")
         exit()
-    check = 0
-    chances+=1
-        
-       
-        
+
+    print(f"Voce tem {10 - chances} chances")
+    print(" ".join(letras_adivinhadas))
     
+    letra_adivinhada = input("Digite uma letra: ")
+    if len(letra_adivinhada) != 1:
+        print("Por favor, digite apenas uma letra.")
+        continue
+    
+    acertou = False
+    for i, letra in enumerate(palavra_secreta):
+        if letra == letra_adivinhada:
+            letras_adivinhadas[i] = letra
+            acertou = True
+    
+    if not acertou:
+        chances += 1
+
+    if "_" not in letras_adivinhadas:
+        print(f"Voce venceu!!!\na resposta era {palavra_secreta}")
+        exit()
